9226585a84d2f630e9dc0cbf4c5d16f5336cdde5
rodcasanova12 <60192482+rodcasanova12@users.noreply.github.com>
2023-08-08 14:43:12 -0400

    ConnectHub

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,27 @@
+// app/layout.tsx
+import '../globals.css'
+import { Inter } from 'next/font/google'
+import { ClerkProvider } from '@clerk/nextjs'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'ConnectHub',
+  description: 'A Next.js 13 Meta Threads APP',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={`${inter.className} bg-dark-1`}>
+            {children}
+            </body>
+      </html>
+    </ClerkProvider>
+  )
+}
