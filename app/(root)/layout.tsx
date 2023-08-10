d5bfa12d567e9366ce360f497297ac973eb950b0
rodcasanova12 <60192482+rodcasanova12@users.noreply.github.com>
2023-08-09 23:45:25 -0400

    fixes

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,47 +1,49 @@
-import { ClerkProvider } from '@clerk/nextjs'
-import '../globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import React from "react";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
 
-import Topbar from '@/components/shared/Topbar'
-import LeftSidebar from '@/components/shared/LeftSidebar'
-import RightSidebar from '@/components/shared/RightSidebar'
-import Bottombar from '@/components/shared/Bottombar'
+import "../globals.css";
+import LeftSidebar from "@/components/shared/LeftSidebar";
+import Bottombar from "@/components/shared/Bottombar";
+import RightSidebar from "@/components/shared/RightSidebar";
+import Topbar from "@/components/shared/Topbar";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
-export const metadata = {
-  title: 'TIE IN',
-  description: 'A Next.js 13 Meta Threads APP',
-}
+export const metadata: Metadata = {
+  title: "Tie In",
+  description: "A Next.js 13 Meta Threads application",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
-      <html lang="en">
-        <body className={`${inter.className}`}>
-            <Topbar />
-            
-            <main className='flex flex-row'>
-              <LeftSidebar />
+    <ClerkProvider
+      appearance={{
+        baseTheme: dark,
+      }}
+    >
+      <html lang='en'>
+        <body className={inter.className}>
+          <Topbar />
 
-              <section className='main-container'>
-                <div className='w-full max-w-4xl'>
-                  {children}
-                </div>
+          <main className='flex flex-row'>
+            <LeftSidebar />
+            <section className='main-container'>
+              <div className='w-full max-w-4xl'>{children}</div>
+            </section>
+            {/* @ts-ignore */}
+            <RightSidebar />
+          </main>
 
-              </section>
-
-              <RightSidebar />
-            </main>
-
-            <Bottombar />
-            </body>
+          <Bottombar />
+        </body>
       </html>
     </ClerkProvider>
-  )
+  );
 }
--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,13 +1,14 @@
-import { OrganizationSwitcher, SignOutButton, SignedIn } from "@clerk/nextjs";
+
 import Image from "next/image";
 import Link from "next/link";
 import { dark } from "@clerk/themes"
+import { OrganizationSwitcher, SignOutButton, SignedIn } from "@clerk/nextjs";
 
 function Topbar() {
     return (
     <nav className="topbar">
         <Link href='/' className="flex items-center gap-4">
-            <Image src="/assets/logo.svg" alt="logo" width={90} height={200} />
+            <Image src="/assets/logo.svg" alt="logo" width={85} height={200} />
             <p className="text-heading3-bold text-light-1 max-xs:hiden"></p>
         </Link>
 
@@ -27,13 +28,13 @@ function Topbar() {
                 </SignedIn>
             </div>
 
-            <OrganizationSwitcher 
+            <OrganizationSwitcher
                 appearance={{
                     baseTheme: dark,
                     elements: {
-                        organizationSwitcherTrigger: "py-2 px-4"
-                    },
-                }}
+                    organizationSwitcherTrigger: "py-2 px-4",
+            },
+          }}
             />
         </div>
     </nav>
