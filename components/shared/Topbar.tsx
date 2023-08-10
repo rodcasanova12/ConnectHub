2f52666e83ca6f2bcab97cba353a5701ecc15285
rodcasanova12 <60192482+rodcasanova12@users.noreply.github.com>
2023-08-09 23:01:30 -0400

    Logo risized

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -7,16 +7,17 @@ function Topbar() {
     return (
     <nav className="topbar">
         <Link href='/' className="flex items-center gap-4">
-            <Image src="/assets/logo.svg" alt="logo" width={28} height={28} />
-            <p className="text-heading3-bold text-light-1 max-xs:hiden">TIE IN</p>
+            <Image src="/assets/logo.svg" alt="logo" width={90} height={200} />
+            <p className="text-heading3-bold text-light-1 max-xs:hiden"></p>
         </Link>
+
         <div className="flex items-center gap-1">
             <div className="block md:hidden">
                 <SignedIn>
                     <SignOutButton>
                         <div className="flex cursor-pointer">
                             <Image
-                                src="assets/logout.svg"
+                                src="/assets/logout.svg"
                                 alt="logout"
                                 width={24}
                                 height={24}
@@ -31,12 +32,12 @@ function Topbar() {
                     baseTheme: dark,
                     elements: {
                         organizationSwitcherTrigger: "py-2 px-4"
-                    }
+                    },
                 }}
             />
         </div>
     </nav>
-  )
+  );
 }
 
 export default Topbar;
